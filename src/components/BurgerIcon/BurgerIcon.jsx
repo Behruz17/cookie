6824d4f4cf1b68f7ee7553e82989ac76f96336d3
Pehhenika <157368481+Pehhenika@users.noreply.github.com>
2024-05-26 22:26:28 +0500

    Delete src/components directory

--- src/components/BurgerIcon/BurgerIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './BurgerIcon.css'
-
-export default function BurgerIcon({onClick, iconSrc}) {
-  return (
-<img onClick={onClick} className='burger-menu' src={iconSrc} alt="" />
-  )
-}
